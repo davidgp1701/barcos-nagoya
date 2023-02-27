79b5339a53807790a654e99c31906ce8c83b160e
David Garcia Perez <david.garcia@weareadaptive.com>
2023-02-27 14:42:11 +0100

    chore: renable portbury

--- barcos/src/run.py
+++ barcos/src/run.py
@@ -3,13 +3,12 @@ import sagunto
 import portbury
 
 
-# Update portbury data
-# portbury.update_forward_movements()
-
 # Update Nagoya port information
 nagoya.update_forward_movements()
-
 
 # Get latests Sagunto ship info
 session = sagunto.setup()
 sagunto.get_next_ships(session)
+
+# Update portbury data
+portbury.update_forward_movements()
